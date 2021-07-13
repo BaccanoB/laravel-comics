03e35910d9d5922a53b4dc46375226dad3409c5d
BaccanoB <beatrice.baccano@yahoo.it>
2021-07-13 16:38:45 +0200

    impostato header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header> 
+    <nav>
+        <img src="{{asset('img/dc-logo.png')}}" alt="">
+        <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>TV</li>
+            <li>Movies</li>
+            <li>Games</li>
+            <li>Collectables</li>
+            <li>Videos</li>
+            <li>Fans</li>
+            <li>News</li>
+            <li>Shop</li>
+        </ul>
+     </nav>
+    <div id="container">
+    </div>
+</header>
